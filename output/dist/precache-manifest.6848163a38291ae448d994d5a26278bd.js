fa79d4cd34d9c53dfa2ffa115ed440b0b1cdb7ad
Matt Gadd <mgadd@sitepen.com>
2018-07-06 11:48:06 +0100

    dist

---
+++ output/dist/precache-manifest.6848163a38291ae448d994d5a26278bd.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = [
+  {
+    "url": "runtime.d41d8cd98f00b204e980.bundle.js"
+  },
+  {
+    "url": "main.467554406151a1f5efa2.bundle.js"
+  },
+  {
+    "revision": "467554406151a1f5efa2",
+    "url": "main.1d4b56a3e984d95a3e50c8455e85fcfb.bundle.css"
+  },
+  {
+    "revision": "3daa9ee3a9bbe667c8b7d9153c519054",
+    "url": "index.html"
+  },
+  {
+    "url": "icon_512x512.a27db8278cda45cc1834378d31dd99b0.png"
+  },
+  {
+    "url": "icon_256x256.bbc561c7de7c17c75c9652f298711dd5.png"
+  },
+  {
+    "url": "icon_128x128.5104764018eed8be9714e699918521db.png"
+  }
+];
