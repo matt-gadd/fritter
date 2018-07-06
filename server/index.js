4fa24efad79ebc7b91e8eb0e4809b1fb346527fe
Anthony Gubler <agubler@sitepen.com>
2018-07-06 09:38:14 +0100

    Update for default fritters and fav API

--- server/index.js
+++ server/index.js
@@ -16,49 +16,100 @@ const upload = multer({ dest: 'uploads/' });
 app.use(cors());
 const fritters = [];
 
-function broadcast(data) {
-	wss.clients.forEach((client) => {
-		if (client.readyState === WebSocket.OPEN) {
-			client.send(JSON.stringify(data));
+function createFritter(id, message, high_quality_url, favCount = 0) {
+	const fileParts = high_quality_url.split('upload');
+	const low_quality_url = `${fileParts[0]}upload/q_15,e_blur:300${fileParts[1]}`;
+	const fritter = {
+		id,
+		low_quality_url,
+		high_quality_url,
+		message,
+		favCount,
+		datetime: Date.now()
+	};
+	return fritter;
+}
+
+async function buildFritters() {
+	const result = await cloudinary.v2.search.expression('folder=default_dogs').sort_by('public_id').max_results(30).execute();
+	for (let i = 0; i < 500; i++) {
+		const index = Math.floor(Math.random() * result.resources.length);
+		const image = result.resources[index];
+		const favCount = Math.floor(Math.random() * 20);
+		const message = 'OMG check this doggy out!';
+		const fritter = createFritter(i, message, image.secure_url, favCount);
+		fritters.push(fritter);
+	}
+}
+
+async function startup() {
+	await buildFritters();
+
+	function broadcast(data) {
+		wss.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(JSON.stringify(data));
+			}
+		});
+	}
+
+	wss.on('connection', (ws, req) => {
+		console.log('Websocket connected', req.connection.remoteAddress);
+		ws.on('error', (error) => {
+			console.log('web socket error', error);
+		});
+
+		ws.on('close', () => {
+			console.log('Websocket closed', req.connection.remoteAddress);
+		});
+	});
+
+	app.post('/messages/upload', upload.single('image'), (req, res) => {
+		cloudinary.v2.uploader.upload(req.file.path, { format: 'jpg', width: 580, height: 400, crop: 'fill' }, (error, result) => {
+			if (error) {
+				return res.sendStatus(500);
+			}
+			const fileParts = result.secure_url.split('upload');
+			const low_quality_url = `${fileParts[0]}upload/q_15,e_blur:300${fileParts[1]}`;
+			const fritter = createFritter(req.body.id, req.body.message, result.secure_url);
+			fritters.push(fritter);
+			broadcast(fritter);
+			res.sendStatus(201);
+			fs.unlink(req.file.path, (err) => {});
+		});
+	});
+
+	app.post('/messages/:id/fav', (req, res) => {
+		const { id } = req.params;
+		let hasFritter = false;
+		for (let i = 0; i < fritters.length; i++) {
+			if (`${fritters[i].id}` === id) {
+				fritters[i].favCount++;
+				hasFritter = true;
+				break;
+			}
 		}
+		if (hasFritter) {
+			res.sendStatus(200);
+		} else {
+			res.sendStatus(404);
+		}
+	});
+
+	app.get('/messages', (req, res) => {
+		const offset = req.query.offset ? parseInt(req.query.offset) : 0;
+		const size = req.query.size ? parseInt(req.query.size) : 5;
+		const frittersResponse = [ ...fritters ].reverse().slice(offset, size + offset);
+
+		res.json({
+			posts: frittersResponse,
+			total: fritters.length
+		});
+	});
+
+	server.listen(process.env.PORT || 5001, () => {
+		console.log(`Server started on port ${server.address().port}`);
 	});
 }
 
-wss.on('connection', (ws, req) => {
-	console.log('Websocket connected', req.connection.remoteAddress);
-	ws.on('error', (error) => {
-		console.log('web socket error', error);
-	});
-
-	ws.on('close', () => {
-		console.log('Websocket closed', req.connection.remoteAddress);
-	});
-});
-
-app.post('/messages/upload', upload.single('image'), (req, res) => {
-	cloudinary.uploader.upload(req.file.path, (result) => {
-		const fileParts = result.secure_url.split('upload');
-		const low_quality_url = `${fileParts[0]}upload/q_1/w_400/h_400/e_blur:300${fileParts[1]}`;
-		const high_quality_url = `${fileParts[0]}upload/w_400/h_400${fileParts[1]}`;
-		const newFritter = { id: req.body.id, low_quality_url, high_quality_url, message: req.body.message, datetime: Date.now() };
-		fritters.push(newFritter);
-		broadcast(newFritter);
-		res.sendStatus(201);
-		fs.unlink(req.file.path, (err) => {});
-	});
-});
-
-app.get('/messages', (req, res) => {
-	const offset = req.query.offset ? parseInt(req.query.offset) : 0;
-	const size = req.query.size ? parseInt(req.query.size) : 30;
-	const frittersResponse = [ ...fritters ].reverse().slice(offset, size + offset);
-
-	res.json({
-		posts: frittersResponse,
-		total: fritters.length
-	});
-});
-
-server.listen(process.env.PORT || 5001, () => {
-	console.log(`Server started on port ${server.address().port}`);
-});
+startup();
